afd8b4740c981faa4555105ea02c725ae7418fc4
Randatic <randatic@gmail.com>
2018-01-30 22:47:30 -0800

    Added OBJ file support and rendering, camera & camera movement

--- KnightEngine/src/engineTester/MainGameLoop.java
+++ KnightEngine/src/engineTester/MainGameLoop.java
@@ -3,11 +3,13 @@ package engineTester;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
 
+import entities.Camera;
 import entities.Entity;
 import models.RawModel;
 import models.TexturedModel;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
+import renderEngine.OBJLoader;
 import renderEngine.Renderer;
 import shaders.StaticShader;
 import textures.ModelTexture;
@@ -20,38 +22,24 @@ public class MainGameLoop {
 		DisplayManager.createDisplay();
 		
 		Loader loader = new Loader();
-		Renderer renderer = new Renderer();
 		StaticShader shader = new StaticShader();
+		Renderer renderer = new Renderer(shader);
 		
-		//OpenGL expects vertices to be defined counterclockwise by default
-		float[] vertices = {
-			    -0.5f, 0.5f, 0f,
-			    -0.5f, -0.5f, 0f,
-			    0.5f, -0.5f, 0f,
-			    0.5f, 0.5f, 0f,
-			  };
-		int[] indices = {
-				0, 1, 3,
-				3, 1, 2
-		};
 		
-		float[] textureCoords = {
-				0,0,
-				0,1,
-				1,1,
-				1,0
-		};
-		
-		ModelTexture texture = new ModelTexture(loader.loadTexture("test_pattern_5"));
-		RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
+		ModelTexture texture = new ModelTexture(loader.loadTexture("jake"));
+		RawModel model = OBJLoader.loadObjModel("jake", loader);
 		TexturedModel texturedModel = new TexturedModel(model, texture);
-		Entity entity = new Entity(texturedModel, new Vector3f(0, 0, 0), 0, 0, 0, 1);
+		Entity entity = new Entity(texturedModel, new Vector3f(0, 0, -5), 0, 0, 0, 1);
+		
+		Camera camera = new Camera();
 		
 		while(!Display.isCloseRequested()) {
-			//entity.increasePosition(0.002f,	0, 0);
-			entity.increaseRotation(0.2f, 0.2f, 0.2f);
+			//entity.increasePosition(0,	0, -0.1f);
+			//entity.increaseRotation(0.2f, 0.2f, 0.2f);
+			camera.move();
 			renderer.prepare();
 			shader.start();
+			shader.loadViewMatrix(camera);
 			renderer.render(entity, shader);
 			shader.stop();
 			DisplayManager.updateDisplay();
